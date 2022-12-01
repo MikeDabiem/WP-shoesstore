1c4bbc571af3b3ededfd06501b6c2d9b83e84f0f
Mike Dabiem <mikedabiem@gmail.com>
2022-12-01 23:11:55 +0200

    backup

--- assets/scripts/ajax.js
+++ assets/scripts/ajax.js
@@ -10,46 +10,51 @@ jQuery(function jq_filter($) {
         }
         $.post(ajaxurl.url, data, function(response) {
             resp_arr = JSON.parse(response);
+            console.log(resp_arr);
 
-            $('.showcase__items')
-                .empty()
-                .animate({opacity: 1}, 300);
+            $('.showcase__items').empty();
 
-            resp_arr.shoes.forEach(item => {
-                $('.showcase__items').append(`<div class="showcase__items-item">
-                        <a href="#" class="item__thumb">
-                            ${item.thumbnail}
-                        </a>
-                        <a href="#" class="item__title">${item.title}</a>
-                        <p class="item__price">$${item.price}</p>
-                        <div class="item__buttons">
-                            <a href="#" class="item__buttons-view">View item</a>
-                            <input class="item__buttons-heart" type="checkbox" id="heart_${item.id}" name="heart">
-                        </div>
-                    </div>`
-                );
-            });
-            heartsHandler();
+            if (resp_arr.shoes.length > 0) {
+                resp_arr.shoes.forEach(item => {
+                    $('.showcase__items').append(`<div class="showcase__items-item">
+                            <a href="#" class="item__thumb">
+                                ${item.thumbnail}
+                            </a>
+                            <a href="#" class="item__title">${item.title}</a>
+                            <p class="item__price">$${item.price}</p>
+                            <div class="item__buttons">
+                                <a href="#" class="item__buttons-view">View item</a>
+                                <input class="item__buttons-heart" type="checkbox" id="heart_${item.id}" name="heart">
+                            </div>
+                        </div>`
+                    );
+                });
+                heartsHandler();
 
-            $('.gender__option').css('display', 'none');
-            resp_arr.gender.forEach(gender => {
-                $('.gender__option').find(`#${gender.slug}`).parent().removeAttr('style');
-            });
+                $('.gender__option').css('display', 'none');
+                resp_arr.gender.forEach(gender => {
+                    $('.gender__option').find(`#${gender.slug}`).parent().removeAttr('style');
+                });
 
-            $('.peculiarities__option').css('display', 'none');
-            resp_arr.peculiarities.forEach(pec => {
-                $('.peculiarities__option').find(`#${pec.slug}`).parent().removeAttr('style');
-            });
+                $('.peculiarities__option').css('display', 'none');
+                resp_arr.peculiarities.forEach(pec => {
+                    $('.peculiarities__option').find(`#${pec.slug}`).parent().removeAttr('style');
+                });
 
-            $('.color__option').css('display', 'none');
-            resp_arr.colors.forEach(color => {
-                $('.color__option').find(`#${color}`).parent().removeAttr('style');
-            });
+                $('.color__option').css('display', 'none');
+                resp_arr.colors.forEach(color => {
+                    $('.color__option').find(`#${color}`).parent().removeAttr('style');
+                });
 
-            $('.size__option').css('display', 'none');
-            resp_arr.sizes.forEach(size => {
-                $('.size__option').find(`#size-option-${$.escapeSelector(size)}`).parent().removeAttr('style');
-            });
+                $('.size__option').css('display', 'none');
+                resp_arr.sizes.forEach(size => {
+                    $('.size__option').find(`#size-option-${$.escapeSelector(size)}`).parent().removeAttr('style');
+                });
+            } else {
+                $('.showcase__items').append(`<h2 class="filter__title no-prod">Nothing found</h2>`);
+            }
+
+            $('.showcase__items').animate({ opacity: 1 }, 300);
         });
     });
 });
